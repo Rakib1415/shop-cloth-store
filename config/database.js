9bb0faf166315fcb094dc83f02ee04c15426572d
Rakib1415 <rakibiuaece@gmail.com>
2022-12-07 13:06:18 +0600

    deploy with render

--- config/database.js
+++ config/database.js
@@ -1,18 +1,30 @@
   // strapi-api/config/database.js
-  module.exports = ({ env }) => ({
+  // module.exports = ({ env }) => ({
+  //   connection: {
+  //     client: 'postgres',
+  //     connection: {
+  //       host: env('DATABASE_HOST', 'localhost'),
+  //       port: env.int('DATABASE_PORT', 5432),
+  //       database: env('DATABASE_NAME', 'clothes'),
+  //       user: env('DATABASE_USERNAME', 'postgres'),
+  //       password: env('DATABASE_PASSWORD', '12345'),
+  //       schema: env('DATABASE_SCHEMA', 'public'), // Not required
+  //       // ssl: {
+  //       //   rejectUnauthorized: env.bool('DATABASE_SSL_SELF', true),
+  //       // },
+  //     },
+  //     debug: false,
+  //   },
+  // });
+  
+  const path = require('path');
+
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'sqlite',
     connection: {
-      client: 'postgres',
-      connection: {
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'clothes'),
-        user: env('DATABASE_USERNAME', 'postgres'),
-        password: env('DATABASE_PASSWORD', '12345'),
-        schema: env('DATABASE_SCHEMA', 'public'), // Not required
-        // ssl: {
-        //   rejectUnauthorized: env.bool('DATABASE_SSL_SELF', true),
-        // },
-      },
-      debug: false,
+      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
     },
-  });
+    useNullAsDefault: true,
+  },
+});
